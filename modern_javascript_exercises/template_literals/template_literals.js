da9e08021962ea59ac4ecfdb79d10466952372ae
Ellie Judge <judge_ellie@hotmail.com>
2022-11-22 16:18:08 +0000

    adds solution comment

--- modern_javascript_exercises/template_literals/template_literals.js
+++ modern_javascript_exercises/template_literals/template_literals.js
@@ -1,8 +1,11 @@
 // Instructions can be found in template_literals.md
 
 // *Original function below*
+
 // const greeting = (name, dayOfTheWeek) =>
 //   "Hello " + name + "! It is now " + dayOfTheWeek;
+
+// *Solution*
 
 const greeting = (name, dayOfTheWeek) =>
   `Hello ${name}! It is now ${dayOfTheWeek}`;
