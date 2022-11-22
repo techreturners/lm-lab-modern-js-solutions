e806e127b79c9b15992229ba32bab6a0e1bdfbc7
Ellie Judge <judge_ellie@hotmail.com>
2022-11-22 16:39:19 +0000

    adds arrow function solution

--- modern_javascript_exercises/ternaries/ternaries.js
+++ modern_javascript_exercises/ternaries/ternaries.js
@@ -1,14 +1,21 @@
 // Instructions can be found in ternaries.md
 
 // *Original function below*
+
 // function sayHello(name) {
 //   if (name === undefined) {
 //     return "Hello you!";
 //   } else return "Hello, " + name + "!";
 // }
 
+// *Solution*
+
 function sayHello(name) {
   return !name ? "Hello you!" : "Hello, " + name + "!";
 }
+
+// Or using a modern js arrow function with implicit return
+
+const sayHello = (name) => (!name ? "Hello you!" : "Hello, " + name + "!");
 
 module.exports = sayHello;
