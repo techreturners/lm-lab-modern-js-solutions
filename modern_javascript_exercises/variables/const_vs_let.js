6ea60db1d4bc196173e9b425bef4ac1e2ca05cb2
Ellie Judge <judge_ellie@hotmail.com>
2022-11-22 17:26:42 +0000

    updates consts_vs_let to reflect changed source code

--- modern_javascript_exercises/variables/const_vs_let.js
+++ modern_javascript_exercises/variables/const_vs_let.js
@@ -1,13 +1,23 @@
 // Instructions can be found in const_vs_let.md
 
-let count = 0;
-
-const people = ["Barbara", "Jacob", "Angela", "Brangela"];
+// *Original Code*
 
 function countThePeople() {
+  const count = 0;
+  let people = ["Barbara", "Jacob", "Angela", "Brangela"];
   people.forEach(() => (count += 1));
+  return count;
 }
 
-countThePeople();
+// console.log(countThePeople()); // <---- uncomment me!
 
-console.log(count); // <---- uncomment me!
+// *Solution*
+
+function countThePeople() {
+  let count = 0;
+  const people = ["Barbara", "Jacob", "Angela", "Brangela"];
+  people.forEach(() => (count += 1));
+  return count;
+}
+
+console.log(countThePeople()); // <---- uncomment me!
